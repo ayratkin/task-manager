656fadb282b874d6d5953f936575ae532073169a
Ayrat <ayrat.klim1@yandex.ru>
2023-01-31 15:05:39 +0300

    added redux persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,38 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 import cardSlice from "./cardSlice.ts";
 
-let store = configureStore({
-	reducer: {
-		cards: cardSlice
-	},
+const rootReducer = combineReducers({
+	cards: cardSlice
 })
+
+const persistConfig = {
+	key: 'root',
+	storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+})
+
+export const persistor = persistStore(store);
 
 export default store;
