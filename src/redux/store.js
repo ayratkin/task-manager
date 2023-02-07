6d1855722ea654b0b15c72abe23c79ee8ecb10e6
Ayrat <ayrat.klim1@yandex.ru>
2023-02-07 21:44:46 +0300

    added persist store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,29 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
-import {
-	persistStore,
-	persistReducer,
-	FLUSH,
-	REHYDRATE,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
-} from "redux-persist";
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import cardSlice from "./cardSlice.ts";
+import cardSlice from './cardSlice.ts'
 
 const rootReducer = combineReducers({
-	cards: cardSlice
+  cards: cardSlice,
 })
 
 const persistConfig = {
-	key: 'root',
-	storage
+  key: 'root',
+  storage,
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 let store = configureStore({
-	reducer: persistedReducer,
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
-export const persistor = persistStore(store);
+export const persistor = persistStore(store)
 
-export default store;
+export default store
