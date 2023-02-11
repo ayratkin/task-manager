08cac0d923e39a0d03611f718ddc41e57535e30c
Ayrat <ayrat.klim1@yandex.ru>
2023-02-11 16:14:01 +0300

    minor changes: changed the tabs

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,54 +1,54 @@
-import styles from './Task.module.scss'
-import {useDispatch, useSelector} from "react-redux";
+import styles from "./Task.module.scss";
+import { useDispatch, useSelector } from "react-redux";
 import React from "react";
-import trash from '../../images/trash.svg'
-import {changeTaskText, deleteTask} from "../../redux/cardSlice.ts";
+import trash from "../../images/trash.svg";
+import { changeTaskText, deleteTask } from "../../redux/cardSlice.ts";
 
 const Task = (props) => {
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const myRef = React.createRef()
+  const myRef = React.createRef();
 
-	// Печать текста заголовка карточки
-	const changeText = () => {
-		dispatch(changeTaskText({
-			text: myRef.current.value,
-			taskId: props.taskId,
-			cardId: props.cardId,
-		}))
-	}
+  // Печать текста заголовка карточки
+  const changeText = () => {
+    dispatch(changeTaskText({
+      text: myRef.current.value,
+      taskId: props.taskId,
+      cardId: props.cardId
+    }));
+  };
 
-	const deleteMyTask = () => {
-		dispatch(deleteTask({
-			taskId: props.taskId,
-			cardId: props.cardId,
-		}))
-	}
+  const deleteMyTask = () => {
+    dispatch(deleteTask({
+      taskId: props.taskId,
+      cardId: props.cardId
+    }));
+  };
 
-	return (
-		<>
-		<div className={styles.task}>
-			<input
-					className={styles.taskInput}
-					type="text"
-					placeholder={'Enter a title for this card...'}
-					value={props.taskText}
-					ref={myRef}
-					onChange={changeText}
-			/>
-			<button
-				className={styles.deleteTaskBtn}
-				onClick={deleteMyTask}>
-				<img
-					src={trash}
-					alt=""
-					className={styles.trashImg}
-				/>
-			</button>
-		</div>
-		</>
-	)
-}
+  return (
+    <>
+      <div className={styles.task}>
+        <input
+          className={styles.taskInput}
+          type="text"
+          placeholder={"Enter a title for this card..."}
+          value={props.taskText}
+          ref={myRef}
+          onChange={changeText}
+        />
+        <button
+          className={styles.deleteTaskBtn}
+          onClick={deleteMyTask}>
+          <img
+            src={trash}
+            alt=""
+            className={styles.trashImg}
+          />
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Task;
